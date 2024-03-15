4bf668b45f21f1f61c2576618dbefa3a37a49b4b
Rex Smith <rex.smith17@gmail.com>
2024-03-15 10:57:05 -0700

    Updated focus urls to be the name of the focus instead of ID

--- project/focuses/urls.py
+++ project/focuses/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("<int:focus_id>/", views.detail, name="detail"),
+    path("<str:focus_name>/", views.detail, name="detail"),
 ]
